11be8b2a3dfbc879cd8cc99d5cbc753a33c78a23
chuross <azarasi.mo.mo@gmail.com>
2020-09-20 23:58:06 +0900

    add test message

--- pkg/service/line/reply.go
+++ pkg/service/line/reply.go
@@ -4,5 +4,6 @@ import "github.com/line/line-bot-sdk-go/linebot"
 
 func ReplyLineMessages() ([]linebot.SendingMessage, error) {
 	messages := make([]linebot.SendingMessage, 0)
+	messages = append(messages, linebot.NewTextMessage("pong"))
 	return messages, nil
 }
